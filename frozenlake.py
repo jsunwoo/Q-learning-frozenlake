b5c0f9e4c5e6e05dd8e0797ebbdd26343e26ee50
jsunwoo <jinsung.sunwoo@gmail.com>
2018-03-08 00:54:36 +0900

    Change rl mode

--- frozenlake.py
+++ frozenlake.py
@@ -1,66 +1,50 @@
 import gym
+import numpy as np
+import matplotlib.pyplot as plt
 from gym.envs.registration import register
-import sys, tty, termios
+import random as pr
 
-# 키 받아오기 getChar
-class _GetCh:
-    def __call__(self):
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(3)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
-inkey = _GetCh()
+def rargmax(vector):
+    m = np.amax(vector)
+    indices = np.nonzero(vector == m)[0]
+    return pr.choice(indices)
 
-# MACROS
-LEFT = 0
-DOWN = 1
-RIGHT = 2
-UP = 3
-
-# Key mapping
-arrow_keys = {
-    '\x1b[A': UP,
-    '\x1b[B': DOWN,
-    '\x1b[C': RIGHT,
-    '\x1b[D': LEFT,
-}
-
-# 설정하는 부분
 register(
     id='FrozenLake-v3',
     entry_point='gym.envs.toy_text:FrozenLakeEnv',
     kwargs={'map_name': '4x4', 'is_slippery': False}
 )
 
-# environment를 만들어주기
 env = gym.make("FrozenLake-v3")
+
 # initial board
-# render는 말그대로 environment를 그려주는 역할
 env.render()
 
-while True:
-    # 키를 받아주고
-    key = inkey()
+# initialize table with all zeros
+# space : 16, action : 4
+Q = np.zeros([env.observation_space.n, env.action_space.n])
 
-    # 키가 만약 상하좌우가 아니면 게임 종료
-    if key not in arrow_keys.keys():
-        print("Game aborted!")
-        break
+# Set learning parameters
+num_episodes = 100
 
-    # action에 방향키를 받아온다
-    action = arrow_keys[key]
-    # action을 취함으로써 정보들을 갱신해준다
-    state, reward, done, info = env.step(action)
-    env.render()
-    # 헷갈리지 않게 정보를 계속 출력해주기 위함
-    print("State: ", state, "Action: ", action,
-          "Reward: ", reward, "Info: ", info)
+rList = []
+for i in range(num_episodes):
+    # Observe current state
+    state = env.reset()
+    rAll = 0
+    done = False
+    while not done:
+        # if Q values are same then pick the way randomly
+        action = rargmax(Q[state, :])
+        # update new information
+        new_state, reward, done, info = env.step(action)
+        # Q(s,a) <- r + maxQ(s', a')
+        Q[state, action] = reward + np.max(Q[new_state, :])
+        rAll += reward
+        # s <- s'
+        state = new_state
+    rList.append(rAll)
+    print(" Left Down Right Up")
+    print(Q)
 
-    # 게임이 종료되었을 경우에 해당
-    if done:
-        print("Finished with reward", reward)
-        break
+print("Success rate : " + str(sum(rList)/num_episodes))
