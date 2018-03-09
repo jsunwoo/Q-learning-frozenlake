66b14209916771f5edcf15066b326dc8e891217f
jsunwoo <jinsung.sunwoo@gmail.com>
2018-03-09 22:44:03 +0900

    Set e-greedy

--- frozenlake.py
+++ frozenlake.py
@@ -25,16 +25,12 @@ env.render()
 # space : 16, action : 4
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 
-# Discount factor (to find optimal(shortest) path)
-dis = 0.99
+# Discount factor (to find optimal(shorter) path)
+dis = 0.9
+
 # Set learning parameters
-num_episodes = 100
+num_episodes = 1000
 rList = []
-
-#a = np.arange(6).reshape(2,3)
-#print(a)
-#print(np.argmax(a))
-
 
 for i in range(num_episodes):
     # Observe current state
@@ -42,27 +38,11 @@ for i in range(num_episodes):
     rAll = 0
     done = False
     while not done:
-        e= np.random.rand(1)
-        if e < 0.2:
+        # E-greedy (to explore)
+        if np.random.rand(1) > 1.0 / (i + 1):
             action = rargmax(Q[state, :])
         else:
             action = env.action_space.sample()
-        print(e, action)
-
-        # action choose with decaying noise (to explore other way)
-        #action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i + 1))
-
-        #print(Q)
-
-        #if np.random.rand(1) < e:
-        #e = np.random.rand(1)
-
-        #action = rargmax(Q[state, :])
-        #print(Q[state, :])
-        #print(action)
-        #print(np.argmax(Q[state, :]))
-        #print(np.random.randn(1, env.action_space.n))
-        #print(Q[state, :] + np.random.randn(1, env.action_space.n) / (i + 1))
 
         # update new information
         new_state, reward, done, info = env.step(action)
@@ -73,15 +53,15 @@ for i in range(num_episodes):
 
         # s <- s'
         state = new_state
-        print "\nLeft(0) Down(1) Right(2) Up(3)",
-        for o in range(16):
-            if o % 4 == 0:
-                print ''
-            print "(",
-            for a in range(4):
-                print("%.2f" % Q[o][a]),
-            print ")",
     rList.append(rAll)
 
-
-print("\nSuccess rate : " + str(sum(rList)/num_episodes))
+print "Left(0) Down(1) Right(2) Up(3)\n",
+for o in range(16):
+    if o % 4 == 0:
+        print ''
+    print "(",
+    for a in range(4):
+        print("%.2f" % Q[o][a]),
+    print ")",
+print("\n\nSuccess rate : " + str(sum(rList)/num_episodes))
+print("Number of episodes : " + str(num_episodes))
